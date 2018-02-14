ce6ed7daf3c4d60406229e706e55e3160803ff6f
Lukasz Czajka <lukaszcz@mimuw.edu.pl>
2018-02-14 19:57:46 +0100

    eval corections

--- eval/atp/run-provers.sh
+++ eval/atp/run-provers.sh
@@ -6,15 +6,15 @@ do
     echo $d
     rm i/f
     ln -s ../$d i/f
-    make -k -j 45 e-19 vam-40 z3-40q
+    make -k -j "$1" e-19 vam-40 z3-40q
     p=`basename $d`
     mv o/e-19 o/eprover-$p
     mv o/vam-40 o/vampire-$p
     mv o/z3-40q o/z3-$p
-    if [ -n "$1" ]; then
-        echo "" | mail -s "$p finished" $1
+    if [ -n "$2" ]; then
+        echo "" | mail -s "$p finished" "$2"
     fi
 done
-if [ -n "$1" ]; then
-    echo "" | mail -s "Provers finished" $1
+if [ -n "$2" ]; then
+    echo "" | mail -s "Provers finished" "$2"
 fi
--- eval/run-reconstr.sh
+++ eval/run-reconstr.sh
@@ -3,5 +3,6 @@
 for d in atp/o/*; do
     s=`basename $d`
     echo "$s" > coqhammer.opt
-    coqc "$1" >> "$2" 2>&1
+    make -k -j "$1" reconstr
+    mv logs/reconstr logs/reconstr-$s
 done
