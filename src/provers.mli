3c2ea382f6604b434bfd4b3a6a6e016c6291be34
Lukasz Czajka <lukaszcz@mimuw.edu.pl>
2018-08-27 16:14:22 +0200

    fix compilation

--- src/provers.mli
+++ src/provers.mli
@@ -1,9 +1,9 @@
-
 open Hh_term
 
 val extract_eprover_data : string (* file name *) -> string list (* deps *) * string list (* defs *)
 val extract_vampire_data : string (* file name *) -> string list (* deps *) * string list (* defs *)
 val extract_z3_data : string (* file name *) -> string list (* deps *) * string list (* defs *)
+val extract_cvc4_data : string (* file name *) -> string list (* deps *) * string list (* defs *)
 
 val write_atp_file : string (* file name *) -> hhdef list (* filtered defs *) ->
   hhdef list (* hyps *) -> hhdef list (* all defs *) -> hhdef (* goal *) -> unit
