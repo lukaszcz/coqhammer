2ae9fc91ed062f4d7d2a2847e39146fdee41e408
Karl Palmskog <palmskog@gmail.com>
2019-05-21 14:53:20 -0500

    Separate OPAM packages for hammer and tactics, update CI for 8.9

---
+++ default.nix
@@ -0,0 +1,28 @@
+{ pkgs ? (import <nixpkgs> {}), coq-version-or-url, shell ? false }:
+
+let
+  coq-version-parts = builtins.match "([0-9]+).([0-9]+)" coq-version-or-url;
+  coqPackages =
+    if coq-version-parts == null then
+      pkgs.mkCoqPackages (import (fetchTarball coq-version-or-url) {})
+    else
+      pkgs."coqPackages_${builtins.concatStringsSep "_" coq-version-parts}";
+in
+
+with coqPackages;
+
+pkgs.stdenv.mkDerivation {
+
+  name = "coqhammer";
+
+  buildInputs = with coq.ocamlPackages; [ ocaml findlib camlp5 ]
+    ++ pkgs.lib.optionals shell [ merlin ocp-indent ocp-index ];
+
+  propagatedBuildInputs = [
+    coq
+  ];
+
+  src = if shell then null else ./.;
+
+  installFlags = [ "BINDIR=$(out)/bin/" "COQLIB=$(out)/lib/coq/${coq.coq-version}/" ];
+}
