3e59dbedcde29aa50b74129b8ad827887cef5cb9
Lukasz Czajka <lukaszcz@mimuw.edu.pl>
2019-05-27 15:52:45 +0200

    run-eval.sh verbose mode

--- eval/run-eval.sh
+++ eval/run-eval.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+VERBOSE=""
+if [ "$1" = "-v" ]; then
+    VERBOSE="-v"
+    shift
+fi
 ./gen-atp.sh $1 $2
 cd atp
-./run-provers.sh $1 $2
+if [ "$VERBOSE" = "-v" ]; then
+    ./run-provers.sh $1 $2
+else
+    ./run-provers.sh $1
+    if [ -n "$2" ]; then
+        echo "" | mail -s "Provers finished" "$2"
+    fi
+fi
 cd ..
 ./run-reconstr.sh $1 $2
 ./gen-stats.sh
