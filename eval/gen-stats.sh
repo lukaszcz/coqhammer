502aa5a36f9bc5f104b03eff6f08e30b2f065e33
Lukasz Czajka <lukaszcz@mimuw.edu.pl>
2019-11-25 16:01:18 +0100

    eval/gen-stats.sh

--- eval/gen-stats.sh
+++ eval/gen-stats.sh
@@ -9,43 +9,22 @@ cd ..
 tools/stat -r , y,p , , false
 
 echo "<table>" >> statistics.html
-echo "<tr><th>reasy</th><th>rsimple</th><th>rcrush</th></tr>" >> statistics.html
+echo "<tr><th>hauto</th><th>hauto 4000</th><th>scrush</th><th></th></tr>" >> statistics.html
 echo "<tr>" >> statistics.html
 echo "<td>" >> statistics.html
-echo `find out -name "*.out" -exec grep reasy {} + | wc -l` >> statistics.html
+echo `find out -name "*.out" -exec grep 'hauto$' {} + | wc -l` >> statistics.html
 echo "</td>" >> statistics.html
 echo "<td>" >> statistics.html
-echo `find out -name "*.out" -exec grep rsimple {} + | wc -l` >> statistics.html
+echo `find out -name "*.out" -exec grep 'hauto 4000' {} + | wc -l` >> statistics.html
 echo "</td>" >> statistics.html
 echo "<td>" >> statistics.html
-echo `find out -name "*.out" -exec grep rcrush {} + | wc -l` >> statistics.html
-echo "</td>" >> statistics.html
-echo "</tr>" >> statistics.html
-echo "</table>" >> statistics.html
-
-echo "<table>" >> statistics.html
-echo "<tr><th>ryelles4</th><th>rblast</th><th>ryreconstr</th><th>rreconstr4</th><th>ryelles6</th><th>rexhaustive1</th><th>rscrush</th></tr>" >> statistics.html
-echo "<tr>" >> statistics.html
-echo "<td>" >> statistics.html
-echo `find out -name "*.out" -exec grep ryelles4 {} + | wc -l` >> statistics.html
+echo `find out -name "*.out" -exec grep scrush {} + | wc -l` >> statistics.html
 echo "</td>" >> statistics.html
 echo "<td>" >> statistics.html
-echo `find out -name "*.out" -exec grep rblast {} + | wc -l` >> statistics.html
+echo `find out -name "*.out" -exec grep sprover {} + | wc -l` >> statistics.html
 echo "</td>" >> statistics.html
 echo "<td>" >> statistics.html
-echo `find out -name "*.out" -exec grep ryreconstr {} + | wc -l` >> statistics.html
-echo "</td>" >> statistics.html
-echo "<td>" >> statistics.html
-echo `find out -name "*.out" -exec grep rreconstr4 {} + | wc -l` >> statistics.html
-echo "</td>" >> statistics.html
-echo "<td>" >> statistics.html
-echo `find out -name "*.out" -exec grep ryelles6 {} + | wc -l` >> statistics.html
-echo "</td>" >> statistics.html
-echo "<td>" >> statistics.html
-echo `find out -name "*.out" -exec grep rexhaustive1 {} + | wc -l` >> statistics.html
-echo "</td>" >> statistics.html
-echo "<td>" >> statistics.html
-echo `find out -name "*.out" -exec grep rscrush {} + | wc -l` >> statistics.html
+echo `find out -name "*.out" -exec grep hprover {} + | wc -l` >> statistics.html
 echo "</td>" >> statistics.html
 echo "</tr>" >> statistics.html
 echo "</table>" >> statistics.html
