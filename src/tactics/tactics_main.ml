f8b897b44fc6fe052d1b13f920c0bbb125ca5377
Lukasz Czajka <lukaszcz@mimuw.edu.pl>
2019-11-26 17:49:29 +0100

    sauto: eager rewriting

--- src/tactics/tactics_main.ml
+++ src/tactics/tactics_main.ml
@@ -93,6 +93,7 @@ let get_s_opts ropts bases unfoldings inverting ctrs =
     | "no_simple_invert" -> { opts with s_simple_inverting = false }
     | "no_eager_invert" -> { opts with s_eager_inverting = false }
     | "no_eager_reduction" -> { opts with s_eager_reducing = false }
+    | "no_eager_rewrite" -> { opts with s_eager_rewriting = false }
     | "no_reflect" -> { opts with s_bnat_reflect = false }
     | "exhaustive" -> { opts with s_exhaustive = true }
     | "default" -> opts
