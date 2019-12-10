59f41472b6f81f6a9eabea065ef8a08ee0fb6199
Lukasz Czajka <lukaszcz@mimuw.edu.pl>
2019-12-10 10:00:07 +0100

    tprover

--- theories/Tactics/Tactics.v
+++ theories/Tactics/Tactics.v
@@ -963,6 +963,5 @@ Tactic Notation "tprove" int_or_var(i) :=
   unshelve (sauto_gen i with (nohints) unfolding logic inverting (logic, @eq) ctrs (logic, @eq) opts no_eager_invert no_case_split no_simple_split no_reduction no_eager_rewrite no_reflect tree_cost_model exhaustive); dsolve.
 
 Ltac tprover :=
-  solve [ tprove 200 | tprove 1000 | tprove 4000 | tprove 12000 |
-          tprove 40000 | tprove 120000 | tprove 400000 | tprove 1200000 |
-          tprove 4000000 | tprove 12000000 | tprove 40000000 | tprove 120000000 ].
+  solve [ tprove 400 | tprove 4000 | tprove 12000 | tprove 40000 | tprove 120000 | tprove 400000 |
+          tprove 1200000 | tprove 4000000 | tprove 12000000 | tprove 40000000 | tprove 120000000 ].
