c5b59fc6d688d36182c655a1b4494d4056a2da2a
Lukasz Czajka <lukaszcz@mimuw.edu.pl>
2019-12-10 17:13:01 +0100

    eval/run-prove.sh

--- eval/run-prove.sh
+++ eval/run-prove.sh
@@ -3,6 +3,12 @@
 make clean-vo
 echo "prove" > coqhammer.opt
 make -k -j "$1" prove
+
+echo -n "Total problems: "
+ls out/*.out | wc -l
+echo -n "Successes: "
+grep "^Success " out/*.out | wc -l
+
 if [ -n "$2" ]; then
     echo "" | mail -s "Coq proving finished" "$2"
 fi
