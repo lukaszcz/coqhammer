5b310fd63c3b09e779f771a054af3bddde3a416f
Lukasz Czajka <lukaszcz@mimuw.edu.pl>
2019-12-30 17:04:49 +0100

    reconstruction tactics

--- src/plugin/hammer_main.ml
+++ src/plugin/hammer_main.ml
@@ -316,8 +316,8 @@ let check_goal_prop gl =
 let run_tactics args msg_success msg_fail =
   let tactics = [
     [ ("rhauto", "hauto"); ("reauto", "xeauto"); ("rscrush", "scrush"); ("rqcrush", "qcrush") ];
-    [ ("rleauto", "leauto"); ("rqprover", "qprover"); ("rsyelles", "syelles"); ("rqcrush2", "qcrush2") ];
-    [ ("rqblast", "qblast"); ("rsprover", "sprover"); ("rsblast", "sblast"); ("rsreconstr", "sreconstr") ]
+    [ ("rleauto", "leauto"); ("rqprover", "qprover"); ("rsyelles", "syelles"); ("rsreconstr", "sreconstr") ];
+    [ ("rqblast", "qblast"); ("rqcrush2", "qcrush2"); ("rsblast", "sblast"); ("rhcrush", "hcrush") ]
   ]
   in
   let ltacs = List.map (List.map (fun tac -> (Utils.ltac_eval (fst tac) args, snd tac))) tactics
