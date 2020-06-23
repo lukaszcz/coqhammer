3c17b36c92bd19ab6012d3525cb2ced3ca64d6f4
Lukasz Czajka <lukaszcz@mimuw.edu.pl>
2020-06-23 22:38:50 +0200

    sauto speedup with proper coq API comparisons

--- src/lib/hhlpo.ml
+++ src/lib/hhlpo.ml
@@ -1,19 +1,21 @@
 (* Lexicographic path order on terms -- implementation *)
 
+open Names
+
 module Utils = Hhutils
 
 let gt cgt evd =
   let rec gt t1 t2 =
-    let ge t1 t2 = t1 = t2 || gt t1 t2 in
-    let (h1, args1) = Utils.destruct_app evd t1 in
-    let (h2, args2) = Utils.destruct_app evd t2 in
     let open Constr in
     let open EConstr in
+    let ge t1 t2 = eq_constr evd t1 t2 || gt t1 t2 in
+    let (h1, args1) = Utils.destruct_app evd t1 in
+    let (h2, args2) = Utils.destruct_app evd t2 in
     match kind evd h1, kind evd h2 with
-    | Const (c1, _), Const(c2, _) when c1 = c2 ->
+    | Const (c1, _), Const(c2, _) when Constant.equal c1 c2 ->
        let rec go args1 args2 =
          match args1, args2 with
-         | a1 :: args1', a2 :: args2' when a1 = a2 ->
+         | a1 :: args1', a2 :: args2' when eq_constr evd a1 a2 ->
             go args1' args2'
          | a1 :: args1', a2 :: args2' when gt a1 a2 ->
             List.for_all (gt t1) args2'
@@ -48,7 +50,7 @@ let rec const_gt c1 c2 =
                  begin fun _ acc t ->
                  let open Constr in
                  match kind t with
-                 | Const(c, _) when c <> c1 -> c :: acc
+                 | Const(c, _) when not (Constant.equal c c1) -> c :: acc
                  | _ -> acc
                  end
                  []
@@ -57,7 +59,7 @@ let rec const_gt c1 c2 =
              let rec go lst =
                match lst with
                | c :: lst' ->
-                  if c = c2 || const_gt c c2 then
+                  if Constant.equal c c2 || const_gt c c2 then
                     true
                   else
                     go lst'
