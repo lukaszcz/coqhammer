b1ab8fe756c090e3e8dd3f5c1a3a00613e55c044
Lukasz Czajka <lukaszcz@mimuw.edu.pl>
2020-06-30 20:35:15 +0200

    'use' tactic

--- src/tactics/tactics_main.ml
+++ src/tactics/tactics_main.ml
@@ -321,3 +321,9 @@ let try_usolve (opts : s_opts) (lst : sopt_t list) (ret : s_opts -> unit Proofvi
             (fun _ -> Tacticals.New.tclZEROMSG (Pp.str msg))
         end
   end
+
+let use_lemmas lst =
+  let use_tac t =
+    Utils.ltac_eval "Tactics.use_tac" [Tacinterp.Value.of_constr t]
+  in
+  List.fold_left (fun tac t -> tac <*> use_tac t) Tacticals.New.tclIDTAC lst
