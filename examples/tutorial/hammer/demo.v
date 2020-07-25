dd49f12259406d60aff7ca08ce8b028ffd20ef2b
Lukasz Czajka <lukaszcz@mimuw.edu.pl>
2020-07-25 15:43:19 +0200

    demo

--- examples/tutorial/hammer/demo.v
+++ examples/tutorial/hammer/demo.v
@@ -40,6 +40,18 @@ Proof.
   strivial use: List.incl_appr, List.incl_refl, List.incl_appl.
 Qed.
 
+Lemma lem_lst_1 : forall (A : Type) (l l' : list A), List.NoDup (l ++ l') -> List.NoDup l.
+Proof.
+  (* The "hammer" tactic can't do induction. If induction is necessary
+  to carry out the proof, then one needs to start the induction
+  manually. *)
+  induction l'.
+  - (* hammer. *)
+    scongruence use: List.app_nil_end.
+  - (* hammer. *)
+    srun eauto use: List.NoDup_remove_1.
+Qed.
+
 Require Import Sorting.Permutation.
 
 Lemma lem_perm_1 {A} : forall (x y : A) l1 l2 l3,
@@ -73,33 +85,6 @@ Proof.
   pattern (y :: l3).
   rewrite List.app_comm_cons.
   apply lem_perm_2; assumption.
-Qed.
-
-Lemma lem_perm_5 {A} : forall (x y z : A) l1 l2 l3 l,
-    Permutation (x :: l1 ++ l2) (y :: l3) ->
-    Permutation (z :: l ++ x :: l1 ++ l2) (y :: z :: l ++ l3).
-Proof.
-  (* hammer. *)
-  intros *.
-  intro H.
-  rewrite List.app_comm_cons.
-  eapply perm_trans.
-  - eapply Permutation_app; [ apply Permutation_refl | eassumption ].
-  - rewrite List.app_comm_cons.
-    apply Permutation_sym.
-    apply Permutation_middle.
-Qed.
-
-Lemma lem_lst_1 : forall (A : Type) (l l' : list A), List.NoDup (l ++ l') -> List.NoDup l.
-Proof.
-  (* The "hammer" tactic can't do induction. If induction is necessary
-  to carry out the proof, then one needs to start the induction
-  manually. *)
-  induction l'.
-  - (* hammer. *)
-    scongruence use: List.app_nil_end.
-  - (* hammer. *)
-    srun eauto use: List.NoDup_remove_1.
 Qed.
 
 (*
