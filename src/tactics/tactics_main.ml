d9f0ddf792ffd290f36eeb874a225dae2f2c9b23
Yann Leray <yann.leray@inria.fr>
2024-02-05 12:24:26 +0100

    Adapt to coq/coq#18038 (rewrite rules)

--- src/tactics/tactics_main.ml
+++ src/tactics/tactics_main.ml
@@ -27,6 +27,8 @@ let with_delayed_uconstr ist c tac =
     expand_evars = true;
     program_mode = false;
     polymorphic = false;
+    undeclared_evars_patvars = false;
+    patvars_abstract = false;
   } in
   let c = Tacinterp.type_uconstr ~flags ist c in
   Tacticals.tclDELAYEDWITHHOLES true c tac
