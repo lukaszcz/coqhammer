bea91da82082d0836841e671b9de724e7eefe419
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2026-06-11 12:05:18 +0200

    Adapt to rocq-prover/rocq#22114.

--- src/plugin/hammer_main.ml
+++ src/plugin/hammer_main.ml
@@ -92,7 +92,7 @@ let rec hhterm_of (t : Constr.t) : Hh_term.hhterm =
      tuple [mk_id "$App"; hhterm_of f; hhterm_of_constrarray args]
   | Const (c,u)       -> hhterm_of_constant c
   | Proj (p,_,c)        -> tuple [mk_id "$Proj";
-                                hhterm_of_constant (Projection.constant p);
+                                mk_id (string_of_int (Projection.arg p));
                                 hhterm_of_bool (Projection.unfolded p);
                                 hhterm_of c]
   | Evar (evk,cl)     -> raise (HammerError "Existential variables not supported.")
